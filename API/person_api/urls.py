625a0f4bb2fd6da697ee2b3843804dff189307ec
PedroAlvim <pedroab.alvim@gmail.com>
2021-05-17 13:21:16 -0300

    fix: router in person_api.urls.py

--- API/person_api/urls.py
+++ API/person_api/urls.py
@@ -4,13 +4,11 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'universities', views.UniversityViewSet, basename='Universities')
+router.register(r'universities', views.UniversityViewSet, basename='universities')
 router.register(r'search', views.UniversityAPIView, basename='search')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', views.UniversityAPIView.as_view),
+    path('', include(router.urls))
 ]
